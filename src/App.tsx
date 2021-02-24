a17bdae83ff0c71cf87962f5b9070477c1f38959
Luka-dot <79lukask@gmail.com>
2021-02-24 14:38:08 -0500

    bosic set up completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+interface AppProps {
+  color: string
+}
+
+class App extends React.Component<AppProps> {
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{this.props.color}</div>
     </div>
   );
+  }
 }
 
 export default App;
